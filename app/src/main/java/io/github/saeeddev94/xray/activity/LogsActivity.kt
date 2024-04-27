b75e51b62ad9060edeef4ba3be582d3d182e0406
Eken Chan <maskedeken@yahoo.com>
2024-04-27 14:04:40 +0800

    show logs dynamically

--- app/src/main/java/io/github/saeeddev94/xray/activity/LogsActivity.kt
+++ app/src/main/java/io/github/saeeddev94/xray/activity/LogsActivity.kt
@@ -1,13 +1,14 @@
 package io.github.saeeddev94.xray.activity
 
+import android.annotation.SuppressLint
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
-import android.widget.ScrollView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
@@ -17,11 +18,18 @@ import io.github.saeeddev94.xray.databinding.ActivityLogsBinding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.nio.charset.StandardCharsets
 
 class LogsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLogsBinding
-    private var menu: Menu? = null
+
+    companion object {
+        private const val MAX_BUFFERED_LINES = (1 shl 14) - 1
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,63 +38,21 @@ class LogsActivity : AppCompatActivity() {
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        lifecycleScope.launch(Dispatchers.IO) { streamingLog() }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_logs, menu)
-        this.menu = menu
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-            R.id.refreshLogs -> {
-                logcat()
-            }
-            R.id.deleteLogs -> {
-                flush()
-            }
             R.id.copyLogs -> copyToClipboard(binding.logsTextView.text.toString())
             else -> finish()
         }
         return true
-    }
-
-    override fun onStart() {
-        super.onStart()
-        logcat()
-    }
-
-    private fun logcat() {
-        binding.pbWaiting.visibility = View.VISIBLE
-        val refreshMenu = menu?.findItem(R.id.refreshLogs)?.setVisible(false)
-        lifecycleScope.launch(Dispatchers.IO) {
-            try {
-                val loggingProcess = ProcessBuilder("logcat", "-d", "-v", "time", "-s", "GoLog,${BuildConfig.APPLICATION_ID}").start()
-                val allText = loggingProcess.inputStream.bufferedReader().use { it.readText() }
-                withContext(Dispatchers.Main) {
-                    binding.logsTextView.text = allText
-                    binding.pbWaiting.visibility = View.GONE
-                    refreshMenu?.setVisible(true)
-                    binding.logsScrollView.post {
-                        binding.logsScrollView.fullScroll(ScrollView.FOCUS_DOWN)
-                    }
-                }
-            } catch (error: Exception) {
-                error.printStackTrace()
-            }
-        }
-    }
-
-    private fun flush() {
-        lifecycleScope.launch(Dispatchers.IO) {
-            val command = listOf("logcat", "-c")
-            val process = ProcessBuilder(command).start()
-            process.waitFor()
-            withContext(Dispatchers.Main) {
-                binding.logsTextView.text = ""
-            }
-        }
     }
 
     private fun copyToClipboard(text: String) {
@@ -101,4 +67,46 @@ class LogsActivity : AppCompatActivity() {
         }
     }
 
+    @SuppressLint("SetTextI18n")
+    private suspend fun streamingLog() = withContext(Dispatchers.IO) {
+        val builder = ProcessBuilder("logcat", "-v", "time", "-s", "GoLog,${BuildConfig.APPLICATION_ID}")
+        builder.environment()["LC_ALL"] = "C"
+        var process: Process? = null
+        try {
+            process = try {
+                builder.start()
+            } catch (e: IOException) {
+                Log.e(packageName, Log.getStackTraceString(e))
+                return@withContext
+            }
+            val stdout = BufferedReader(InputStreamReader(process!!.inputStream, StandardCharsets.UTF_8))
+
+            var timeLastNotify = System.nanoTime()
+            val bufferedLogLines = arrayListOf<String>()
+            var timeout = 1000000000L / 2 // The timeout is initially small so that the view gets populated immediately.
+
+            while (true) {
+                val line = stdout.readLine() ?: break
+                bufferedLogLines.add(line)
+                val timeNow = System.nanoTime()
+                if (bufferedLogLines.size < MAX_BUFFERED_LINES && (timeNow - timeLastNotify) < timeout && stdout.ready())
+                    continue
+                timeout = 1000000000L * 5 / 2 // Increase the timeout after the initial view has something in it.
+                timeLastNotify = timeNow
+
+                withContext(Dispatchers.Main.immediate) {
+                    val isScrolledToBottomAlready = !binding.logsScrollView.canScrollVertically(1)
+                    binding.logsTextView.text = binding.logsTextView.text.toString() + bufferedLogLines.joinToString(separator = "\n", postfix = "\n")
+                    bufferedLogLines.clear()
+                    if (isScrolledToBottomAlready) {
+                        binding.logsScrollView.post {
+                            binding.logsScrollView.fullScroll(View.FOCUS_DOWN)
+                        }
+                    }
+                }
+            }
+        } finally {
+            process?.destroy()
+        }
+    }
 }
